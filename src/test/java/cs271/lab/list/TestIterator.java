50b4cc3b04372b4eb09394d644e86faa2f592736
Exzoz <farisgjaleel@gmail.com>
2018-06-21 02:57:00 +0300

    first commit

---
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -0,0 +1,108 @@
+package cs271.lab.list;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.LinkedList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestIterator {
+
+    private List<Integer> list;
+
+    @Before
+    public void setUp() throws Exception {
+        list = new LinkedList<Integer>();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        list = null;
+    }
+
+    @Test
+    public void testEmpty() {
+        final Iterator<Integer> i = list.iterator();
+        assertFalse(i.hasNext());
+    }
+
+    @Test
+    public void testNonempty() {
+        list.add(33);
+        list.add(77);
+        list.add(44);
+        list.add(77);
+        list.add(55);
+        list.add(77);
+        list.add(66);
+
+        final Iterator<Integer> i = list.iterator();
+
+        assertTrue(i.hasNext());
+        assertEquals(33, i.next().intValue());
+        assertTrue(i.hasNext());
+        assertEquals(77, i.next().intValue());
+        assertTrue(i.hasNext());
+        assertEquals(44, i.next().intValue());
+        assertTrue(i.hasNext());
+        assertEquals(77, i.next().intValue());
+        assertTrue(i.hasNext());
+        assertEquals(55, i.next().intValue());
+        assertTrue(i.hasNext());
+        assertEquals(77, i.next().intValue());
+        assertTrue(i.hasNext());
+        assertEquals(66, i.next().intValue());
+        assertFalse(i.hasNext());
+    }
+
+    @Test
+    public void testRemove() {
+
+        list.add(33);
+        list.add(77);
+        list.add(44);
+        list.add(77);
+        list.add(55);
+        list.add(77);
+        list.add(66);
+
+        final Iterator<Integer> i = list.iterator();
+
+        while (i.hasNext()) {
+            if (i.next() == 77) {
+                i.remove();
+            }
+        }
+        assertEquals(Arrays.asList(33, 44, 55, 66), list);
+    }
+
+    @Test
+    public void testAverageValues() {
+        list.add(33);
+        list.add(77);
+        list.add(44);
+        list.add(77);
+        list.add(55);
+        list.add(77);
+        list.add(66);
+
+        double sum = 0;
+        int n = 0;
+
+        final Iterator<Integer> i = list.iterator();
+
+        while (i.hasNext()) {
+            n++;
+            sum += i.next();
+        }
+
+        assertEquals(61.3, sum / n, 0.1);
+        assertEquals(7, n);
+    }
+}
---
+++ src/test/java/cs271/lab/list/TestList.java
@@ -0,0 +1,209 @@
+package cs271.lab.list;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestList {
+
+  private List<Integer> list;
+
+  @Before
+  public void setUp() throws Exception {
+    list = new ArrayList<Integer>();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    list = null;
+  }
+
+  @Test
+  public void testSizeEmpty() {
+    assertTrue(list.isEmpty());
+    assertEquals(0, list.size());
+    try {
+      list.get(0);
+      fail("there should not be any items in the list");
+    } catch (Exception ex) {
+    }
+  }
+
+  @Test
+  public void testSizeNonEmpty() {
+    list.add(77);
+    assertEquals(false, list.isEmpty());
+    assertEquals(1, list.size());
+    assertEquals(77, list.get(0).intValue());
+  }
+
+  @Test
+  public void testContains() {
+    assertEquals(false, list.contains(77));
+    list.add(77);
+    assertEquals(true, list.contains(77));
+  }
+
+
+  @Test
+  public void testAddMultiple() {
+    list.add(77);
+    list.add(77);
+    list.add(77);
+
+    assertEquals(3, list.size());
+    assertEquals(0, list.indexOf(77));
+    assertEquals(77, list.get(1).intValue());
+    assertEquals(2, list.lastIndexOf(77));
+  }
+
+  @Test
+  public void testAddMultiple2() {
+    list.add(33);
+    list.add(77);
+    list.add(44);
+    list.add(77);
+    list.add(55);
+    list.add(77);
+    list.add(66);
+
+    assertEquals(7, list.size());
+    assertEquals(1, list.indexOf(77));
+    assertEquals(5, list.lastIndexOf(77));
+    assertEquals(44, list.get(2).intValue());
+    assertEquals(77, list.get(3).intValue());
+    assertEquals(Arrays.asList(33, 77, 44, 77, 55, 77, 66), list);
+  }
+
+  @Test
+  public void testRemoveObject() {
+    list.add(3);
+    list.add(77);
+    list.add(4);
+    list.add(77);
+    list.add(5);
+    list.add(77);
+    list.add(6);
+    list.remove(5);
+
+    assertEquals(6, list.size());
+    assertEquals(1, list.indexOf(77));
+    assertEquals(3, list.lastIndexOf(77));
+    assertEquals(4, list.get(2).intValue());
+    assertEquals(77, list.get(3).intValue());
+    list.remove(Integer.valueOf(5));
+    assertEquals(5, list.size());
+    assertEquals(1, list.indexOf(77));
+    assertEquals(3, list.lastIndexOf(77));
+    assertEquals(4, list.get(2).intValue());
+    assertEquals(77, list.get(3).intValue());
+  }
+
+  @Test
+  public void testContainsAll() {
+    list.add(33);
+    list.add(77);
+    list.add(44);
+    list.add(77);
+    list.add(55);
+    list.add(77);
+    list.add(66);
+
+    assertEquals(true, list.containsAll(Arrays.asList(33, 44, 55, 66, 77)));
+    assertEquals(false, list.containsAll(Arrays.asList(11, 22, 33)));
+  }
+
+  @Test
+  public void testAddAll() {
+    list.addAll(Arrays.asList(33, 77, 44, 77, 55, 77, 66));
+
+    assertEquals(7, list.size());
+    assertEquals(33, list.get(0).intValue());
+    assertEquals(77, list.get(1).intValue());
+    assertEquals(44, list.get(2).intValue());
+    assertEquals(77, list.get(3).intValue());
+    assertEquals(55, list.get(4).intValue());
+    assertEquals(77, list.get(5).intValue());
+    assertEquals(66, list.get(6).intValue());
+  }
+
+  @Test
+  public void testRemoveAll() {
+
+    list.add(33);
+    list.add(77);
+    list.add(44);
+    list.add(77);
+    list.add(55);
+    list.add(77);
+    list.add(66);
+
+    list.removeAll(Arrays.asList(33, 44, 55, 66));
+
+    assertEquals(3, list.size());
+    assertEquals(Arrays.asList(77, 77, 77), list);
+  }
+
+  @Test
+  public void testRetainAll() {
+
+    list.add(33);
+    list.add(77);
+    list.add(44);
+    list.add(77);
+    list.add(55);
+    list.add(77);
+    list.add(66);
+
+    list.retainAll(Arrays.asList(77));
+
+    assertEquals(3, list.size());
+    assertEquals(Arrays.asList(77, 77, 77), list);
+  }
+
+  @Test
+  public void testSet() {
+
+    list.add(33);
+    list.add(77);
+    list.add(44);
+    list.add(77);
+    list.add(55);
+    list.add(77);
+    list.add(66);
+
+    list.set(1, 99);
+    list.set(3, 99);
+    list.set(5, 99);
+
+    assertEquals(7, list.size());
+    assertEquals(33, list.get(0).intValue());
+    assertEquals(99, list.get(1).intValue());
+    assertEquals(44, list.get(2).intValue());
+    assertEquals(99, list.get(3).intValue());
+    assertEquals(55, list.get(4).intValue());
+    assertEquals(99, list.get(5).intValue());
+    assertEquals(66, list.get(6).intValue());
+  }
+
+  @Test
+  public void testSubList() {
+
+    list.add(33);
+    list.add(77);
+    list.add(44);
+    list.add(77);
+    list.add(55);
+    list.add(77);
+    list.add(66);
+
+    assertEquals(Arrays.asList(44, 77, 55), list.subList(2, 5));
+  }
+}
